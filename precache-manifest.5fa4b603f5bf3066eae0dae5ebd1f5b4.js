9ca956ace1b8d291bd668e400bc2a0f04aaf5fea
bogdanovs <sergey.bogdanov@geropharm.com>
2020-09-29 01:35:31 +0300

    Updates

---
+++ precache-manifest.5fa4b603f5bf3066eae0dae5ebd1f5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd73c52eca73334c072fb7d3e5d6d034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "632038b8ca56d9ed76e1",
+    "url": "/static/css/main.90630128.chunk.css"
+  },
+  {
+    "revision": "da370a183e0a6ce35d7d",
+    "url": "/static/js/2.9b073f77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9b073f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632038b8ca56d9ed76e1",
+    "url": "/static/js/main.b8e01fd6.chunk.js"
+  },
+  {
+    "revision": "15fa150f3c542184932c",
+    "url": "/static/js/runtime-main.0a3bfc1e.js"
+  }
+]);
